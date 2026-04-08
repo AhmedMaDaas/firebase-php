440556b75df41d703694017c828fd7aa2378ce52
Ahmed Daas <ahmedhamadah973@gmail.com>
2026-04-08 10:35:08 +0400

    fix unpack array with string keys error

--- src/Factory.php
+++ src/Factory.php
@@ -454,7 +454,7 @@ final class Factory
         $config ??= [];
         $middlewares ??= [];
 
-        $config = [...$this->httpClientOptions->guzzleConfig(), ...$config];
+        $config = array_merge($this->httpClientOptions->guzzleConfig(), $config);
 
         $handler = HandlerStack::create($config['handler'] ?? null);
 
